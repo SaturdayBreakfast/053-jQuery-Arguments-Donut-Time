948bd956f67067db79b4c8e2e5074bbd23865f44
SaturdayBreakfast <32108233+SaturdayBreakfast@users.noreply.github.com>
2018-11-08 09:58:44 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$("#yes").click(function() {
+    $("#response").css("background-image","url(http://4.bp.blogspot.com/-j4glvUKje7M/TyZGANn-jOI/AAAAAAAAAHY/bqbswRlo6ss/s1600/homer_donuts.jpg)");
+});
+
+$("#no").click(function() {
+    $("#response").css("background-image","url('https://i.ytimg.com/vi/20HcrNAJZZM/hqdefault.jpg')");
+});
+    
+    $("#donut").click(function() {
+    $("body").css("background-image","url(https://cdn1.benzinga.com/files/imagecache/1024x768xUP/images/story/2012/background-15226_640.jpg)");
+});
+
+
